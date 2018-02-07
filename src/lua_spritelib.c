192194a704baa1fce7588a16ad5da21568caed82
PrisimaTheFox <prisimathefox@gmail.com>
2018-02-07 11:49:19 -0600

    Create references to lua_spritelib.c (#4)
    * Update SRB2.cbp
    
    * Update Srb2.dev
    
    * Update CMakeLists.txt
    
    * Create lua_spritelib.c
    
    * Update Srb2SDL-vc10.vcxproj
    
    * Update Srb2SDL-vc10.vcxproj.filters
    
    * Update project.pbxproj
    
    * Update project.pbxproj
    
    * Update Srb2win-vc10.vcxproj
    
    * Update Srb2win-vc10.vcxproj.filters
    
    * Update Srb2win-vc9.vcproj

---
+++ src/lua_spritelib.c
@@ -0,0 +1,20 @@
+// SONIC ROBO BLAST 2
+//-----------------------------------------------------------------------------
+// Copyright (C) 2018 by John "JTE" Muniz.
+// Copyright (C) 2018 by Sonic Team Junior.
+//
+// This program is free software distributed under the
+// terms of the GNU General Public License, version 2.
+// See the 'LICENSE' file for more details.
+//-----------------------------------------------------------------------------
+/// \file  lua_spritelib.c
+/// \brief custom sprite rendering library for Lua scripting
+
+#include "doomdef.h"
+#ifdef HAVE_BLUA
+#include "lua_script.h"
+#include "lua_libs.h"
+
+
+
+#endif
